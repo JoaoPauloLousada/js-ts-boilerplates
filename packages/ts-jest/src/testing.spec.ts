6ebdf1587ca268a315fcf6c9db9a30d476beee34
Joao Lousada <joao.lousada@crosslend.com>
2022-08-08 10:10:56 +0200

    Add ts-jest boilerplate

---
+++ packages/ts-jest/src/testing.spec.ts
@@ -0,0 +1,10 @@
+describe('sample testing', () => {
+  it('should pass', () => {
+    expect(true).toBeTruthy()
+  })
+
+
+  it.skip('should be skipped', () => {
+    expect(true).toBeTruthy()
+  })
+})
